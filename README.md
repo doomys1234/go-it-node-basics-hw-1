d0a0fca1409ee4018198baf2c6e0d7299b1c0171
doomys1234 <doomys1234@gmail.com>
2022-06-08 22:28:01 +0300

    finished

---
+++ README.md
@@ -0,0 +1,4 @@
+1. action list https://monosnap.com/file/3uDK3YETH3esFlssKPSCuc0vxlUnvY
+2. action get by id https://monosnap.com/file/MWxpuMK4j37Q1ysdD7jhBui7fAsv0o
+3. action add new contact https://monosnap.com/file/UNOt9hkU5WkKQlsj43NIAwCFjicgEI
+4. remove contact https://monosnap.com/file/lfGf8ihnY7o053lQuwr6p5mYJbY6uE
---
+++ contacts.js
@@ -0,0 +1,63 @@
+const fs = require("fs").promises;
+const path = require("path");
+const contactsPath = path.resolve("./db/contacts.json");
+
+function listContacts() {
+    fs.readFile(contactsPath)
+    .then(data => console.log(data.toString()))
+    .then(error => console.log(error))
+}
+
+
+function getContactById(contactId) {
+
+    fs.readFile(contactsPath)
+        .then(data => {
+            const arr = JSON.parse(data)
+            return arr.filter(item=> parseInt(item.id)=== contactId)
+    })
+    .then(error => console.log(error))
+
+}
+
+
+
+function removeContact(contactId) {
+
+     fs.readFile(contactsPath)
+        .then(data => {
+            const arr = JSON.parse(data)
+            return arr.filter(item=> parseInt(item.id)!== contactId)
+    })
+    .then(error => console.log(error))
+
+}
+
+
+
+function addContact(id, name, email, phone) {
+  fs.readFile(contactsPath)
+    .then((data) => {
+      const arr = JSON.parse(data);
+      const newItem = {
+        id: arr.length + 1,
+        name: name,
+        email: email,
+        phone: phone,
+      };
+      arr.push(newItem);
+      fs.writeFile(contactsPath, JSON.stringify(arr), (err) => {
+        if (err) {
+          console.log(err);
+        }
+      });
+    })
+    .catch((error) => console.log(error));
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+};
